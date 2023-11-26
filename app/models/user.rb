d85b52c53230cd79dfa990a5d9a5a9974d40253e
Santonero <rbsanto@proton.me>
2023-11-26 18:23:55 +0100

    Add email regex formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid adress" }
+  validates :password, :password_confirmation, presence: true
   normalizes :email, with: -> email { email.strip.downcase }
 
   generates_token_for :password_reset, expires_in: 10.minutes do
