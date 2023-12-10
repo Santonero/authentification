c1b9c67ff2cb6e29bfa7c1c9824815b8351a8d96
Santonero <rbsanto@proton.me>
2023-12-10 13:11:21 +0100

    Prevent querying database if there is no user.id in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def authenticate_from_session
-    User.find_by(id: session[:user_id])
+    User.find_by(id: session[:user_id]) if session[:user_id].present?
   end
 
   def user_signed_in?
